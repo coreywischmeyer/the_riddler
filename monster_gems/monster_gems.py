a8f48d13b4a749e998e5a617f6799e5aee2c2c45
Corey Wischmeyer <cwischmeyer@oriongenomics.com>
2016-05-20 11:01:54 -0500

    Added the riddler prompt.

--- monster_gems/monster_gems.py
+++ monster_gems/monster_gems.py
@@ -1,9 +1,19 @@
+'''
+A video game requires you to slay monsters to collect gems.
+Every time you slay a monster, it drops one of three types of gems:
+a common gem, an uncommon gem or a rare gem. The probabilities of
+these gems being dropped are in the ratio of 3:2:1 — three common
+gems for every two uncommon gems for every one rare gem, on average.
+If you slay monsters until you have at least one of each of the three
+types of gems, how many of the most common gems will you end up with, on average?
+'''
 import random
 
+common = [i for i in range(1, 4)]
+uncommon = [i for i in range(4, 6)]
+rare = [6]
+
 def slay_monsters():
-  common = [i for i in range(1, 4)]
-  uncommon = [i for i in range(4, 6)]
-  rare = [6]
   gem_bag = []
   all_collected = False
   while(all_collected is False):
